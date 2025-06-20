e490c9ff3e9db5d363ad75bd6b7e0d1a6da73c89
OMG <oals666@naver.com>
2025-06-20 15:44:40 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/chatx_match/ChatxMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatx_match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatxMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatxMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatx_match/ChatxMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatx_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatxMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
